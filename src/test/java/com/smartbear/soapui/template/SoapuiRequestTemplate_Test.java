f228c3f0ceac53c50c9d112d66976c933dd66eab
hnxyhcwdl1003@163.com <@vincent0506#>
2018-11-30 20:28:34 +0800

    修改

--- src/test/java/com/smartbear/soapui/template/SoapuiRequestTemplate_Test.java
+++ src/test/java/com/smartbear/soapui/template/SoapuiRequestTemplate_Test.java
@@ -16,14 +16,17 @@
 package com.smartbear.soapui.template;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.xmlbeans.XmlException;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.eviware.soapui.SoapUI;
 import com.eviware.soapui.model.iface.Request.SubmitException;
 import com.eviware.soapui.model.iface.Response;
+import com.eviware.soapui.settings.HttpSettings;
 import com.eviware.soapui.support.SoapUIException;
 import com.google.common.collect.Maps;
 
@@ -34,9 +37,10 @@ public class SoapuiRequestTemplate_Test extends AbstractWsdlTemplate_Test {
 	@Before
 	public void setupRequestTemplate() throws Exception {
 		requestTemplate = new SoapuiRequestTemplate(wsdlTemplate);
+		SoapUI.getSettings().setBoolean(HttpSettings.DISABLE_RESPONSE_DECOMPRESSION, false);
 	}
 	
-	@Test
+	//@Test
 	public void invokeAt() throws XmlException, IOException, SoapUIException, SubmitException {
 
 		//SoapUI.getSettings().setBoolean(HttpSettings.DISABLE_RESPONSE_DECOMPRESSION, true);
@@ -56,7 +60,7 @@ public class SoapuiRequestTemplate_Test extends AbstractWsdlTemplate_Test {
 		
 	}
 	
-	@Test
+	//@Test
 	public void invokeByName() throws XmlException, IOException, SoapUIException, SubmitException {
 		
 		//SoapUI.getSettings().setBoolean(HttpSettings.DISABLE_RESPONSE_DECOMPRESSION, true);
@@ -67,6 +71,27 @@ public class SoapuiRequestTemplate_Test extends AbstractWsdlTemplate_Test {
 		//params.put("theIpAddress",  "221.110.10.14");
 		
 		Response response = requestTemplate.invokeByName(wsdlUrl, "getCountryCityByIp", "221.110.10.14");
+		
+		System.out.println(response.getResponseHeaders());
+		
+		System.out.println(response.getContentAsString());
+		
+	}
+	
+	@Test
+	public void invokeByName2() throws XmlException, IOException, SoapUIException, SubmitException {
+		
+		//SoapUI.getSettings().setBoolean(HttpSettings.DISABLE_RESPONSE_DECOMPRESSION, true);
+		
+		System.err.println( "invokeByName===========================================");
+		
+		//Map<String, Object> params = Maps.newHashMap();
+		//params.put("theIpAddress",  "221.110.10.14");
+		wsdlUrl = "http://ws.webxml.com.cn/WebServices/MobileCodeWS.asmx?wsdl";
+		Map<String, Object> requestParams = new HashMap<>();
+		requestParams.put("mobileCode", "13735893463");
+		
+		Response response = requestTemplate.invokeByName(wsdlUrl, "getMobileCodeInfo", requestParams);
 		
 		System.out.println(response.getResponseHeaders());
 		
